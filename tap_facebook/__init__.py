bfc31001c067d79c8e1934e2f65eb8792c0b9abe
Lidiya Hanicheva <lidiya.hanicheva@health-union.com>
2026-07-29 17:25:32 +0300

    DATA-12015: Investigate Pipeline Backoff Behavior

--- tap_facebook/__init__.py
+++ tap_facebook/__init__.py
@@ -125,7 +125,7 @@ def retry_on_summary_param_error(backoff_type, exception, **wait_gen_kwargs):
 original_call = FacebookAdsApi.call
 
 @retry_on_summary_param_error(backoff.expo, (FacebookRequestError), max_tries=5, factor=5)
-@retry_on_adreport_job_not_ready_error(backoff.constant, (FacebookRequestError), max_tries=5, interval=30)
+@retry_on_adreport_job_not_ready_error(backoff.expo, (FacebookRequestError), max_tries=5, factor=5)
 def call_with_retry(self, method, path, params=None, headers=None, files=None, url_override=None, api_version=None,):
     """
     Adding the retry decorator on the original function call
